4ce85a265392d5ea0d8ed47210219d077f015487
MRobot404 <qomc16@gmail.com>
2021-09-26 17:23:54 -0600

    Primer Commit con datos de API en pantalla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController, Platform, IonRouterOutlet} from '@ionic/angular';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private alertController: AlertController
+  ) {}
+
+  backButtonEvent() {
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      this.backButtonAlert();
+    });
+  }
+
+  async backButtonAlert() {
+    const alert = await this.alertController.create({
+      message: 'Acabas de presionar el boton de retroceso',
+    });
+    await alert.present();
+  }
 }
