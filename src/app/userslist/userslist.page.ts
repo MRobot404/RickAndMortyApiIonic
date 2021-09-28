39e2c111a04176925f92833d2119cab767b93f1b
MRobot404 <qomc16@gmail.com>
2021-09-27 18:22:14 -0600

    Segundo commit lista de personajes con detalle

--- src/app/userslist/userslist.page.ts
+++ src/app/userslist/userslist.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-userslist',
@@ -8,17 +9,38 @@ import {HttpClient} from '@angular/common/http';
 })
 export class UserslistPage implements OnInit {
   characters =[];
+  page=0;
+  maximumPages=3;
   constructor(
  private http: HttpClient
-  ) {}
-
+  ) {
+    this.loadUsers();
+  }
   ngOnInit() {
-    this.http.get<any>('https://rickandmortyapi.com/api/character')
+
+  }
+
+
+  loadUsers(event?) {
+    this.http.get<any>('https://rickandmortyapi.com/api/character/?results=20&page=1')
     .subscribe(res =>{
       console.log(res);
-      this.characters = res.results;
+      this.characters = this.characters.concat(res.results);
+
+      if(event){
+        event.target.complete();
+      }
     });
   }
 
+  loadMore(event){
+    console.log(event);
+    this.page=this.page+1;
+    this.loadUsers(event);
+
+    if(this.page === this.maximumPages){
+      event.target.disabled=true;
+    }
+  }
 
 }
